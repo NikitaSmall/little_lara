26592a0ee54691ae1b0688bc93919ccfe9b71197
nikitasmall <nikitasosnov@yahoo.com>
2015-07-20 11:21:28 +0300

    added errors handling for login and registration

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,15 @@
 @section('content')
 <h1>Вход в систему</h1>
 <br>
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
   {!! Form::open(array('url' => '/auth/login')) !!}
     {!! csrf_field() !!}
     <div class="form-group">
